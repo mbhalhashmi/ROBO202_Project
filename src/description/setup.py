ad46034f76ff293ead54d12c298ce74b77bc30c5
mbhalhashmi <mbh.alhashmi@gmail.com>
2025-11-18 20:18:31 +0400

    Fixed launch issues and tricycle urdf issues

--- src/description/setup.py
+++ src/description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'description'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share',package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,7 +28,6 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'exe = pubsub_description.print_val:main',
         ],
     },
 )
