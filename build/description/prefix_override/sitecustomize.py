08672bd157cb498a65761a377ba52355ac10cfe8
mbhalhashmi <mbh.alhashmi@gmail.com>
2025-11-26 01:51:52 +0400

    fixed camera positioning

--- build/description/prefix_override/sitecustomize.py
+++ build/description/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mohammed/ROBO202_Project/install/description'
+    sys.prefix = sys.exec_prefix = '/home/mohamed/ROBO202_Project/install/description'
